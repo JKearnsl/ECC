6b814751aaeb8b5a319c14dab3de7cce5d3c199f
JKearnsl <boyarshinovn@gmail.com>
2023-12-25 19:04:10 +0300

    Добавлена таблица BCH полиномов, изменен нейминг

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,7 +1,6 @@
 from prettytable import PrettyTable
-import numpy as np
 
-DICT_POLYNOM = {
+DICT_POLYNOM_CYCLIC = {
     1: (1, "x + 1", "11", (3, 2)),
     2: (2, "x^2 + x + 1", "111", (3, 1)),
     3: (3, "x^3 + x^2 + 1", "1101", (7, 4)),
@@ -14,17 +13,31 @@ DICT_POLYNOM = {
     10: (5, "x^5 + x^3 + 1", "101001", (31, 26))
 }
 
+DICT_POLYNOM_BCH = {
+    1: (3, "x^3 + x + 1", "1011", (7, 4, 1)),
+    2: (8, "x^8 + x^7 + x^6  x^4 + 1", "111010001", (15, 7, 2)),
+}
 
-def polynom_table() -> str:
+
+def polynom_cyclic_table() -> str:
     pretty_table = PrettyTable()
     pretty_table.field_names = ["№", "r-степень полинома", "Порождающий полином", "Запись по mod 2", "(n, k)"]
     pretty_table.align = "l"
-    for index, values in DICT_POLYNOM.items():
+    for index, values in DICT_POLYNOM_CYCLIC.items():
         pretty_table.add_row([index, values[0], values[1], values[2], values[3]])
     return pretty_table.get_string()
 
 
-def build_syndrome_matrix(get_syndrome, polynom):
+def polynom_bch_table() -> str:
+    pretty_table = PrettyTable()
+    pretty_table.field_names = ["№", "r-степень полинома", "Порождающий полином", "Запись по mod 2", "(n, k, t)"]
+    pretty_table.align = "l"
+    for index, values in DICT_POLYNOM_BCH.items():
+        pretty_table.add_row([index, values[0], values[1], values[2], values[3]])
+    return pretty_table.get_string()
+
+
+def build_syndrome_cyclic_matrix(get_syndrome, polynom):
     n = len(polynom) - 1
 
     # Матрица (2^n - 1) x n
@@ -41,8 +54,8 @@ def build_syndrome_matrix(get_syndrome, polynom):
     return matrix
 
 
-def syndrome_table(get_syndrome, polynom):
-    matrix = build_syndrome_matrix(get_syndrome, polynom)
+def syndrome_cyclic_table(get_syndrome, polynom):
+    matrix = build_syndrome_cyclic_matrix(get_syndrome, polynom)
 
     pretty_table = PrettyTable()
     pretty_table.field_names = ["№ Ошибки", "Синдром"]
